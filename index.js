06a7ad7c502ee6a7086efca75cda9228302f504e
harlo <harlo.holmes@gmail.com>
2017-04-21 17:14:19 -0400

    node-ified

--- index.js
+++ index.js
@@ -26,5 +26,5 @@ var server = app.listen(8080, function() {
 	var host = server.address().address;
 	var port = server.address().port;
 
-	console.log("Twilio Demo Started at http://%s:%s", host, port);
+	console.log("Node Graph Demo Started at http://%s:%s", host, port);
 });
